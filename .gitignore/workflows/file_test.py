be4aeedf07c5d2e9f9f599d22c1e4a26c0e1e342
Daniel <daniel@datascientest.com>
2022-10-20 09:50:31 +0000

    changement dans file test

--- .gitignore/workflows/file_test.py
+++ .gitignore/workflows/file_test.py
@@ -2,8 +2,8 @@ import pytest
 
 def test_calc_addition():
   # Fonction test du résultat de 2+4
-    output = 2+4
-    assert output == 6
+    output = 2+5
+    assert output == 7
 
 def test_calc_substraction():
   # Fonction test du résultat de 2-4
